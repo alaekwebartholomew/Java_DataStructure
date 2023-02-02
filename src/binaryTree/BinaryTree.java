ac7363df7b8e7f7c68a3b704ca3f605fdfac3df8
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-02 08:48:52 -0800

    Added traversal method

--- src/binaryTree/BinaryTree.java
+++ src/binaryTree/BinaryTree.java
@@ -1,115 +1,71 @@
 package binaryTree;
 
-import java.util.ArrayList;
+public class BinaryTree{
 
-public class BinaryTree implements TreeInterface{
-    BinaryTree left;
-    int e;
-    BinaryTree right;
-    private int size;
+        static class Node {
+            int value;
+            Node left, right;
 
-    public BinaryTree(int e){
-        this.left = null;
-        this.e = e;
-        this.right = null;
-        this.size = 0;
-    }
-
-
-    @Override
-    public void addChild(int e) {
-        if(this.e == e)
-            return;
-        if(this.e > e){
-            if(this.right != null){
-                this.right.addChild(e);
-            }else {
-                this.right = new BinaryTree(e);
-                this.size++;
+            public Node(int data) {
+                this.value = data;
+                this.left = null;
+                this.right = null;
             }
         }
-        if(this.e < e){
-            if(this.left != null){
-                this.left.addChild(e);
-            }else {
-                this.left = new BinaryTree(e);
-                this.size++;
+
+    public void addChild(Node node, int value) {
+        if (value < node.value) {
+            if (node.left != null) {
+                addChild(node.left, value);
+            } else {
+                node.left = new Node(value);
+            }
+        } else if (value > node.value) {
+            if (node.right != null) {
+                addChild(node.right, value);
+            } else {
+                node.right = new Node(value);
             }
         }
     }
 
-    @Override
-    public ArrayList<Integer> preOrderTraversal() {
-        ArrayList<Integer> element = new ArrayList<>();
-        if(this.size == 0)
-            element.add(this.e);
-        if(this.left != null)
-            return this.left.preOrderTraversal();
-        if(this.right != null)
-            return this.right.preOrderTraversal();
-        return element;
+    public void inOrderTraversal(Node node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.print(" " + node.value);
+            inOrderTraversal(node.right);
+        }
     }
 
-    @Override
-    public ArrayList<Integer> postOrderTraversal() {
-        return ;
+    public void preOrderTraversal(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
     }
 
-    @Override
-    public ArrayList<Integer> inOrderTraversal() {
-        return ;
-    }
-
-    @Override
-    public int findMin() {
-        return 0;
-    }
-
-    @Override
-    public int findMax() {
-        return 0;
-    }
-
-    @Override
-    public boolean isInternal() {
-        return false;
-    }
-
-    @Override
-    public boolean isExternal() {
-        return false;
-    }
-
-    @Override
-    public boolean isRoot() {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public int search(int e) {
-        return 0;
-    }
-
-    @Override
-    public void delete(int e) {
-
-    }
-
-    public static BinaryTree buildNode(int[] items){
-        BinaryTree root = new BinaryTree(items[0]);
-        for (int value: items)
-            root.addChild(value);
-        return root;
+    public void postOrderTraversal(Node node) {
+        if (node != null) {
+            postOrderTraversal(node.left);
+            postOrderTraversal(node.right);
+            System.out.print(" " + node.value);
+        }
     }
 
     public static void main(String[] arg){
-        int[] scores = {84, 78, 71, 70, 88, 74, 72};
-        BinaryTree build = BinaryTree.buildNode(scores);
-        build.postOrderTraversal();
+        BinaryTree tree = new BinaryTree();
+        Node root = new Node(73);
+        tree.addChild(root, 78);
+        tree.addChild(root, 71);
+        tree.addChild(root, 70);
+        tree.addChild(root, 88);
+        tree.addChild(root, 74);
+        tree.addChild(root, 72);
+        tree.inOrderTraversal(root);
+        System.out.println("");
+        tree.preOrderTraversal(root);
+        System.out.println("");
+        tree.postOrderTraversal(root);
     }
 }
