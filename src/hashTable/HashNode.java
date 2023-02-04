e3287e21f43e011f55c0b889a82e5d44b24eef63
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-04 06:38:28 -0800

    added add function

--- src/hashTable/HashNode.java
+++ src/hashTable/HashNode.java
@@ -4,9 +4,13 @@ public class HashNode<K, V> {
 
     K key;
     V value;
+    final int hashCode;
 
-    HashNode(K key, V value){
+    HashNode<K, V> next;
+
+    HashNode(K key, V value, int hashCode){
         this.key = key;
         this.value = value;
+        this.hashCode = hashCode;
     }
 }
--- src/hashTable/HashTable.java
+++ src/hashTable/HashTable.java
@@ -2,11 +2,12 @@ package hashTable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HashTable<K, V> {
 
     private ArrayList<HashNode<K, V>> dataBase;
-    private final int capacity;
+    private int capacity;
     private int size;
 
     public HashTable(){
@@ -17,20 +18,63 @@ public class HashTable<K, V> {
         for (int i = 0; i <= this.capacity; i++)
             dataBase.add(null);
     }
-    public int hashCode(String s) {
-        int h=0;
-        for (int i=0; i<s.length( ); i++) {
-            h = (h << 5) | (h >>> 27);
-            h += (int) s.charAt(i);
-        }
-        return h;
+    private int hashCode(K key) {
+        return Objects.hashCode(key);
+    }
+
+    private int getDataBseIndex(K key){
+        int hashKey = this.hashCode(key);
+        int index = hashKey % this.capacity;
+        index = index < 0 ? index * -1 : index;
+        return index;
     }
 
     public int size(){return this.size;}
 
     public boolean isEmpty(){return this.size() == 0;}
 
-    public static void main(String[] args) {
+    public void add(K key, V value){
+        // Find head of chain for given key
+        int index = getDataBseIndex(key);
+        int hashCode = hashCode(key);
+        HashNode<K, V> head = dataBase.get(index);
 
+        // Check if key is already present
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+        // Insert key in chain
+        size++;
+        head = dataBase.get(index);
+        HashNode<K, V> newNode = new HashNode<K, V>(key, value, hashCode);
+        newNode.next = head;
+        dataBase.set(index, newNode);
+
+        // If load factor goes beyond threshold, then
+        // double hash table size
+        if ((1.0 * this.size) / this.capacity >= 0.7) {
+            ArrayList<HashNode<K, V> > temp = this.dataBase;
+            dataBase = new ArrayList<>();
+            this.capacity = 2 * this.capacity;
+            this.size = 0;
+            for (int i = 0; i < this.capacity; i++)
+                this.dataBase.add(null);
+            for (HashNode<K, V> headNode : temp) {
+                while (headNode != null) {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        HashTable<String, String> map = new HashTable<>();
+        map.add("Learn", "Coding");
     }
 }
