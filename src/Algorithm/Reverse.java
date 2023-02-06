74860cd862687db1c27a6de7fe26f85269e95910
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-06 12:03:32 -0800

    Reverse Array

--- src/Algorithm/Reverse.java
+++ src/Algorithm/Reverse.java
@@ -2,12 +2,24 @@ package Algorithm;
 
 public class Reverse<E> {
 
-    public void reverseList(E[] data){
+    public E[] reverseList(E[] data){
         int firstIndex = 0;
         int lastIndex = data.length - 1;
-
+        while (firstIndex < lastIndex){
+            E temp = data[firstIndex];
+            data[firstIndex++] = data[lastIndex];
+            data[lastIndex--] = temp;
+        }
+        return data;
     }
     public static void main(String[] args) {
+        String[] str = {"code", "read", "money"};
+        Integer[] element = {1, 2, 3, 4, 5, 6, 7};
+        Reverse<String> rev = new Reverse<>();
+        String[] data = rev.reverseList(str);
+        for(String value: data){
+            System.out.print(value +", ");
+        }
 
     }
 }
