82096b02b6cfa30e548ce10b2196bdb677ee7788
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-06 11:45:28 -0800

    algorithm setup

---
+++ src/Algorithm/Reverse.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+public class Reverse<E> {
+
+    public void reverseList(E[] data){
+        int firstIndex = 0;
+        int lastIndex = data.length - 1;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
